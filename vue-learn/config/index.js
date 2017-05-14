d5b7353508cb8c5a6a7b06107c04f738179f7c90
dothin <1286513426@qq.com>
2017-05-14 23:33:27 +0800

    隐藏浏览器console，webpack

--- vue-learn/config/index.js
+++ vue-learn/config/index.js
@@ -8,6 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    //productionSourceMap设置成false，可以隐藏console里面的webpack
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
